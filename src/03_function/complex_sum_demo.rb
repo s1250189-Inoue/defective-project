4dc2f629022b1da1c3a3f241e47063a3b3aac3d1
s1250189-Inoue <s1250189@u-aizu.ac.jp>
2017-06-29 18:40:10 +0900

    solved

--- src/03_function/complex_sum_demo.rb
+++ src/03_function/complex_sum_demo.rb
@@ -1,8 +1,8 @@
+def nest_sum(arr)
+  arr.sort
+sum = 0
+for i in (1..arr) do
+   sum += arr.to_a
 
-# complex_sum(2, [1, 2, 3, 4, 5]) =>
-# [1, 3, 5] // select odd numbers.
-# [3, 5, 7] // add n(=2)
-# 15        // sum
-
-def complex_sum(n, arr)
+end
 end
