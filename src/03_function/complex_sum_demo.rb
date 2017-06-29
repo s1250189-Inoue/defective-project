6d1c377b87c1c4c72b68f9dcd109efb5e6863c3c
s1250189-Inoue <s1250189@u-aizu.ac.jp>
2017-06-29 18:55:01 +0900

    fix 03 complex sum demo

--- src/03_function/complex_sum_demo.rb
+++ src/03_function/complex_sum_demo.rb
@@ -1,8 +1,19 @@
-def nest_sum(arr)
-  arr.sort
-sum = 0
-for i in (1..arr) do
-   sum += arr.to_a
-
+def complex_sum(n, arr)
+  odd_nums = []
+for x in arr do
+  if x %2 != 0
+  odd_nums.push(x)
+  end
 end
+
+added_nums = []
+for x in odd_nums do
+  added_nums.push(x + n)
+end
+
+sum = 0
+for x in added_nums
+  sum += x
+end
+sum
 end
